44a0678c5d6fa4c0b3b378cf7efe15ec0a6ea3f1
rootEnginear <rootEnginear@users.noreply.github.com>
2024-01-26 13:40:52 +0700

    fix: proper logo name

--- web/src/components/info.svelte
+++ web/src/components/info.svelte
@@ -1,3 +1,31 @@
+<script lang="ts">
+	const LOGOS = [
+		{ name: 'กลุ่ม 24 มิถุนา ประชาธิไตย', filename: 'june24th' },
+		{ name: 'CALL', filename: 'call' },
+		{ name: 'DRG', filename: 'drg' },
+		{ name: 'ธรรมศาสตร์และการชุมนุม', filename: 'thammasat' },
+		{ name: 'Wevis', filename: 'wevis' },
+		{ name: 'CrCF', filename: 'crcf' },
+		{ name: 'Worker’s Union', filename: 'workersunion' },
+		{ name: 'iLaw', filename: 'ilaw' },
+		{ name: 'HRLA', filename: 'hrla' },
+		{ name: 'NITI hub', filename: 'nitihub' },
+		{ name: 'โมกหลวงริมน้ำ', filename: 'mokelung' },
+		{ name: 'เครือข่ายนักวิชาการเพื่อสิทธิพลเมือง', filename: 'tanc' },
+		{ name: 'ทะลุฟ้า', filename: 'thalufah' },
+		{ name: 'ครช.', filename: 'cpd' },
+		{ name: 'ACTLAB', filename: 'actlab' },
+		{ name: 'Rocket Media Lab', filename: 'rml' },
+		{ name: 'Bloom', filename: 'bloom' },
+		{ name: 'TLHR', filename: 'tlhr' },
+		{ name: 'FreeArts', filename: 'freearts' },
+		{ name: 'เครือข่ายแรงงานเพื่อสิทธิประชาชน', filename: 'lnpr' },
+		{ name: 'Amnesty', filename: 'amnesty' },
+		{ name: 'เฟมินิสต์ปลดแอก', filename: 'feminist' },
+		{ name: 'พิพิธภัณฑ์สามัญชน', filename: 'mph' },
+	];
+</script>
+
 <div
 	class="flex flex-col gap-3 w-full md:max-w-[650px] px-4 pt-6 pb-20 text-neutral"
 >
@@ -8,10 +36,10 @@
 		</h2>
 	</header>
 	<div class="flex justify-center gap-[6px] flex-wrap">
-		{#each Array(23) as _, idx}
+		{#each LOGOS as { name, filename } (name)}
 			<img
-				src="/cooperations/{idx + 1}.svg"
-				alt=""
+				src="/cooperations/{filename}.svg"
+				alt={name}
 				width="60"
 				height="60"
 				loading="lazy"
