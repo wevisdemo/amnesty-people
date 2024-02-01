c370a68fd00e7faf21902269b8bd09f4c785aa4a
petchill <songpon111333@gmail.com>
2024-02-01 10:13:49 +0700

    fix: enable to have space in lastname in form petition

--- models/src/document.ts
+++ models/src/document.ts
@@ -16,7 +16,7 @@ export const documentSchema = object({
 	[PERSONALID_KEY]: string([toTrimmed(), validateCitizenID()]),
 	prefix: optional(string()),
 	firstname: string([toTrimmed(), minLength(1), regex(/^[ก-๙]+$/)]),
-	lastname: string([toTrimmed(), minLength(1), regex(/^[ก-๙]+$/)]),
+	lastname: string([minLength(1), regex(/^[ก-๙]| +$/)]),
 	signature: string('กรุณาลงชื่อ'),
 });
 
