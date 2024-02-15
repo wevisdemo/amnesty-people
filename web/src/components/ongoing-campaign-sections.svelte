b163e3ecb8eb0d30618b58a0eb320191e5eb7e1e
Th1nkK1D <witheep@gmail.com>
2024-02-15 18:20:46 +0700

    feat(web): add address section after campaign ended

--- web/src/components/ongoing-campaign-sections.svelte
+++ web/src/components/ongoing-campaign-sections.svelte
@@ -24,4 +24,18 @@
 	<section id="volunteer" class="flex justify-center">
 		<Volunteer />
 	</section>
+{:else}
+	<section
+		class="flex flex-col justify-center items-center py-6 px-3 bg-white space-y-4"
+	>
+		<h2 class="heading-responsive-01 text-balance leading-none text-center">
+			ที่อยู่สำหรับจัดส่งเอกสาร
+		</h2>
+		<address
+			class="w-full max-w-md p-2 body-02-semibold text-center text-balance not-italic rounded border border-base-300"
+		>
+			ออฟฟิศ iLaw 099-678-6031<br />1111/3 บ้านกลางเมืองรัชดา-ลาดพร้าว<br
+			/>ถ.ลาดพร้าว แขวงจันทรเกษม เขตจตุจักร<br />กทม. 10900
+		</address>
+	</section>
 {/if}
